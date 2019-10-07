a3e8f3c1af516a8610b277393c7825d7342e30de
Bence Gadanyi <29914644+Beenyaa@users.noreply.github.com>
2019-10-07 02:20:19 +0100

    testing almost done

--- Mention_Media_Postgetter.py
+++ Mention_Media_Postgetter.py
@@ -4,7 +4,15 @@ import re
 import base64
 import time
 
+#needed variables
+urlTarget = input("enter username: ")
+baseURL = "https://www.instagram.com/"
+userURL = baseURL + urlTarget +"/?__a=1"
+postURL = ""
+targetUserCommentID = input("enter comment: ")
 
+
+#post ID to shortcode conversion
 def base10_to_base64(num):
     order = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
     short = "";
@@ -16,25 +24,50 @@ def base10_to_base64(num):
 
     return short;
 
-
+#shortcode to ID conversion
 def base64_to_base10(shortcode):
     code = ('' * (12 - len(shortcode))) + shortcode
     print(code)
     return int.from_bytes(base64.b64decode((code + "==").encode(), "-_"), "big")
 
-
+#gets html of new link
 def info_getter(url):
     html = urlopen(url)
     soup = BeautifulSoup(html, "html.parser");
     return soup.extract()
 
+#loop to iterate through all user posts
+def querying(userID,posts,captions,end_cursor):
+    try:
+        print(end_cursor)
 
-urlTarget = "bboyjohnny"
-baseURL = "https://www.instagram.com/"
-userURL = baseURL + urlTarget +"/?__a=1"
-postURL = ""
-targetUserComment = "I don't remeber when i felt so freely at such a big event. See you next year"
-targetUserComment2 = "Workshops with Bruce Almighty"
+
+        for post in range(0, len(posts)):
+            if len(end_cursor) != 0 and (len(posts) - 1) == post:
+
+                userURL = baseURL + "graphql/query/?query_id=17888483320059182&id="+str(captions[0])+"&first=50"+"&after="\
+                          +str(end_cursor[0])
+
+                userpageINFO = info_getter(userURL)
+
+                posts = re.findall(mediaRegex, str(userpageINFO))
+                #mediaID = re.findall(mediaRegex, str(userpageINFO))
+                captions = re.findall(captionRegex, str(userpageINFO))
+                end_cursor = re.findall(end_cursorRegex, str(userpageINFO))
+                querying(userID,posts,captions,end_cursor)
+
+
+            elif (targetUserCommentID in str(captions[post])):
+                print(captions[post])
+
+                postURL = baseURL + "p/" + str(base10_to_base64(int(posts[post]))) + "/"
+                print("mention media post URL: " + postURL)
+                break
+
+    except(IndexError):
+        print("error")
+
+##########################################################################
 
 userpageINFO = info_getter(userURL)
 
@@ -44,49 +77,27 @@ captionRegex = "\"text\":\"(.+?)\"}"
 end_cursorRegex="\"end_cursor\":\"(.+?)\"}"
 
 userID = re.findall(userIDRegex, str(userpageINFO))
+mediaID = re.findall(mediaRegex, str(userpageINFO))
 captions = re.findall(captionRegex, str(userpageINFO))
 posts = re.findall(mediaRegex, str(userpageINFO))
 
 userURL = baseURL+"graphql/query/?query_id=17888483320059182&id="+str(userID[0])+"&first=50"
 
+#update regex format
 mediaRegex = "{\"id\":\"(\d+?)\",\"__typename\":\"Graph\w+?\""
-captionRegex = "\"text\":\"(.+?)\"}"
-end_cursorRegex="\"end_cursor\":\"(.+?)\"}"
 
 userpageINFO = info_getter(userURL)
 
 posts = re.findall(mediaRegex, str(userpageINFO))
+mediaID = re.findall(mediaRegex, str(userpageINFO))
 captions = re.findall(captionRegex, str(userpageINFO))
 end_cursor = re.findall(end_cursorRegex, str(userpageINFO))
 
 start = time.time()
-for post in range(0,len(posts)):
-    print(post)
-    if len(end_cursor) != 0 and (len(posts)-1) == post:
-
-        userURL = baseURL+"graphql/query/?query_id=17888483320059182&id="+str(userID[0])+"&first=50"+"&after="+str(end_cursor[0])
-
-        userpageINFO = info_getter(userURL)
-
-        posts = re.findall(mediaRegex, str(userpageINFO))
-        captions = re.findall(captionRegex, str(userpageINFO))
-        end_cursor = re.findall(end_cursorRegex, str(userpageINFO))
-        post=0
-
-
-    elif (targetUserComment in str(captions[post])):
-        postURL=baseURL+"p/"+str(base10_to_base64(int(posts[post])))+"/"
-        print("mention media post URL: " +postURL)
-
-
-
-    elif (targetUserComment2 in str(captions[post])):
-        postURL=baseURL+"p/"+str(base10_to_base64(int(posts[post])))+"/"
-        print("mention media post URL: " + postURL)
-
+querying(userID,posts,captions,end_cursor);
 end = time.time()
-
 elapsed = end - start
 
 print("")
-print("Total elapsed time: "+str(elapsed));
+print("Total elapsed time: " + str(elapsed) + " seconds");
+
